36a41d077d8d501d292857d7f2cc2943c5b367d1
fong <593476874@qq.com>
2019-01-21 17:36:23 +0800

    ..

--- recommendation/Basic-PNN-Demo/config.py
+++ recommendation/Basic-PNN-Demo/config.py
@@ -1,11 +1,13 @@
 TRAIN_FILE = "data/train.csv"
 TEST_FILE = "data/test.csv"
 
+
 SUB_DIR = "output"
 
 
 NUM_SPLITS = 3
 RANDOM_SEED = 2017
+
 
 # types of columns of the dataset dataframe
 CATEGORICAL_COLS = [
